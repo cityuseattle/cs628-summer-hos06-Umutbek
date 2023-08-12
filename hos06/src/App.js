a9774288023f26550d46eb962b7e6a8aa30818d2
Umutbek <61200692+Umutbek@users.noreply.github.com>
2023-08-12 23:14:50 +0000

    Submission for Module06-Umutbek

---
+++ hos06/src/App.js
@@ -0,0 +1,53 @@
+// Section 7 - usingNavLink.js
+// https://reactrouter.com/en/main/components/nav-link#navlink
+
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Details from './components/Details';
+import Team from './components/Team';
+import Member from './components/Member';
+import './App.css';
+import './index.css'
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <nav className="navbar">
+            <ul>
+              {/* Use NavLink instead of Link */}
+              <li>
+                <NavLink to="/" activeClassName="active">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" activeClassName="active">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/details" activeClassName="active">Details</NavLink>
+              </li>
+              <li>
+                <NavLink to="/team" activeClassName="active">Team</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/details" element={<Details />} />
+              <Route path="/team" element={<Team />}>
+                <Route path="member/:id" element={<Member />} />
+              </Route>
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
